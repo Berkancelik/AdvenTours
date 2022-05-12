53cd3ecf94c7ba4ef7810f2a313bbd032f268145
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-12 16:23:04 +0300

    The comment process has been prepared. Comments will be made with AJAX in the next step.

---
+++ TraversalCoreProje/Controllers/CommentController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult AddComment(Comment p)
+        {
+            p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.CommentStatus = true;
+            commentManager.TAdd(p);
+            return PartialView();
+        }
+    }
+}
