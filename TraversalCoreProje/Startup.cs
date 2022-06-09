9e8535dc1ca83d32cb981e078b5a41997cdfba3c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-09 11:06:19 +0300

    _UserLayout has been created and _HeaderPartial & _NavbarPartial added

--- TraversalCoreProje/Startup.cs
+++ TraversalCoreProje/Startup.cs
@@ -69,6 +69,15 @@ namespace TraversalCoreProje
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
         }
     }
 }
