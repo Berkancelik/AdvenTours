a11c0905d6dfdb836b07b8ae8cd21567f1f74596
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-09 16:07:15 +0300

    Data listing operation completed successfully

---
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [AllowAnonymous]
+    [Area("Member")]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
