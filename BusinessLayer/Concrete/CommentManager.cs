5ea4a1ef319191b82f61f994654bf1e1ff3574ef
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-13 09:22:46 +0300

    BusinessLayer Concrete Refactoring

--- BusinessLayer/Concrete/CommentManager.cs
+++ BusinessLayer/Concrete/CommentManager.cs
@@ -3,9 +3,6 @@ using DataAccessLayer.Abstract;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
