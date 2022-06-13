63d80ed8db67ed5a2890bc65e531134bb9b9d49e
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-13 09:29:34 +0300

    DataAccess Abstract Refactoring

--- DataAccessLayer/Migrations/ContextModelSnapshot.cs
+++ DataAccessLayer/Migrations/ContextModelSnapshot.cs
@@ -4,7 +4,6 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
--- DataAccessLayer/Repository/GenericRepository.cs
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -4,9 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DataAccessLayer.Repository
 {
     public class GenericRepository<T> : IGenericDal<T> where T : class
