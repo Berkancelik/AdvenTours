abda55ff28d5f0ab6865da8d13cc5a4247d35645
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-20 16:32:12 +0300

    Updating profile, picture information with identity

--- TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
@@ -1,6 +1,9 @@
 ﻿using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using TraversalCoreProje.Areas.Member.Models;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
@@ -25,6 +28,32 @@ namespace TraversalCoreProje.Areas.Member.Controllers
             userEditViewModel.PhoneNumber= values.PhoneNumber;
             userEditViewModel.Mail = values.Email;
             return View(userEditViewModel);
+        }
+
+        [HttpPost]
+        public  async Task<IActionResult> Index (UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Image !=null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Image.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var savelocation = resource + "/wwwroot/userimages/" + imageName;
+                var stream = new FileStream(savelocation, FileMode.Create); 
+                await p.Image.CopyToAsync(stream);
+                user.ImageUrl = imageName;
+            }
+
+            user.Name = p.Name;
+            user.Surname=p.Surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password);
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+            return View();
         }
     }
 }
