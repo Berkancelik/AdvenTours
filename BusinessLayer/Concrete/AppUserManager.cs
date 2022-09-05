b0fbc7c920f4c4582320bfe7404ecd772085bce2
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-09-06 00:04:10 +0300

    User architecture added

---
+++ BusinessLayer/Concrete/AppUserManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AppUserManager : IAppUserService
+    {
+        public void TAdd(AppUser t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(AppUser t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppUser TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AppUser> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(AppUser t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
