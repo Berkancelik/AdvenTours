47ae59e6aae2902b69dff3c9aaa47bdc48fb813f
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-09-08 22:44:26 +0300

    Extensions Class created

---
+++ BusinessLayer/Container/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFreamework;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Container
+{
+    public class Extensions
+    {
+        public void ContainerDependencies(IServiceCollection services)
+        {
+
+            services.AddScoped<ICommentService, CommentManager>();
+            services.AddScoped<ICommentDal, EfCommentDal>();
+            services.AddScoped<IDestinationService, DestinationManager>();
+            services.AddScoped<IDestinationDal, EfDestinationDal>();
+            services.AddScoped<IAppUserService, AppUserManager>();
+            services.AddScoped<IAppUserDal, EfAppUserDal>();
+        }
+    }
+}
--- TraversalCoreProje/Startup.cs
+++ TraversalCoreProje/Startup.cs
@@ -40,12 +40,6 @@ namespace TraversalCoreProje
             services
                 .AddControllersWithViews();
 
-            services.AddScoped<ICommentService, CommentManager>();
-            services.AddScoped<ICommentDal, EfCommentDal>();
-            services.AddScoped<IDestinationService, DestinationManager>();
-            services.AddScoped<IDestinationDal, EfDestinationDal>();
-            services.AddScoped<IAppUserService, AppUserManager>();
-            services.AddScoped<IAppUserDal, EfAppUserDal>();
             services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder()
