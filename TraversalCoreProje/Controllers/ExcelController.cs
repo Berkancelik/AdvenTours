ad7b387e1265c27c618e90cfe2ef95274f57db0e
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-09-23 00:02:38 +0300

    DestinnationModel adn Dynamic Excel created

--- TraversalCoreProje/Controllers/ExcelController.cs
+++ TraversalCoreProje/Controllers/ExcelController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TraversalCoreProje.Models;
 
 namespace TraversalCoreProje.Controllers
 {
@@ -28,6 +34,53 @@ namespace TraversalCoreProje.Controllers
 
 
             return View();
+        }
+
+        public List<DestinationModel> DestinationList()
+        {
+            List<DestinationModel> destinationModels = new List<DestinationModel>();
+            using (var c = new Context())
+            {
+                destinationModels = c.Destinations.Select(x => new DestinationModel
+                {
+                    City = x.City,
+                    DayNight = x.DayNight,
+                    Price = x.Price,
+                    Capacity = x.Capacity
+
+                }).ToList();
+
+                return destinationModels;
+            }
+
+            public IActionResult DestinationExcelReport()
+            {
+                using (var workbook = new XLWorkbook())
+                {
+                    var workSheet = workbook.Worksheets.Add("Tur Listesi");
+                    workSheet.Cell(1, 1).Value = "Şehir";
+                    workSheet.Cell(1, 2).Value = "Konaklama Süresi";
+                    workSheet.Cell(1, 3).Value = "Fiyat";
+                    workSheet.Cell(1, 4).Value = "Kapasite";
+
+                    int rowCount = 2;
+                    foreach (var item in DestinationList())
+                    {
+                        workSheet.Cell(rowCount, 1).Value = item.City;
+                        workSheet.Cell(rowCount, 1).Value = item.DayNight;
+                        workSheet.Cell(rowCount, 1).Value = item.Price;
+                        workSheet.Cell(rowCount, 1).Value = item.Capacity;
+                    }
+
+                    using (var stream = new MemoryStream())
+                    {
+                        workbook.SaveAs(stream);
+                        var content = stream.ToArray();
+                        return File(content, "application/vnd.openxmlformats-ofiicedocument.spreadsheetml.sheet",  "YeniListe.xlsx");
+                    }
+                }
+               
+            }
         }
     }
 }
