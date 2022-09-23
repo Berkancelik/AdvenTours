da1c0751f59ed396b6ea461fea3eb70bf59afea3
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-09-23 23:21:33 +0300

    Excel Report has been completed

--- TraversalCoreProje/Controllers/ExcelController.cs
+++ TraversalCoreProje/Controllers/ExcelController.cs
@@ -1,5 +1,6 @@
 ﻿using ClosedXML.Excel;
 using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
 using System.Collections.Generic;
@@ -9,10 +10,32 @@ using TraversalCoreProje.Models;
 
 namespace TraversalCoreProje.Controllers
 {
+    [AllowAnonymous]
     public class ExcelController : Controller
     {
         public IActionResult Index()
         {
+           return View();   
+        }
+
+        public List<DestinationModel> DestinationList()
+        {
+            List<DestinationModel> destinationModels = new List<DestinationModel>();
+            using (var c = new Context())
+            {
+                destinationModels = c.Destinations.Select(x => new DestinationModel
+                {
+                    City = x.City,
+                    DayNight = x.DayNight,
+                    Price = x.Price,
+                    Capacity = x.Capacity
+
+                }).ToList();
+            }
+            return destinationModels;
+        }
+
+        public IActionResult StaticExcelReport() {
             ExcelPackage excel = new ExcelPackage();
             var workSheet = excel.Workbook.Worksheets.Add("Sayfa1");
             workSheet.Cells[1, 1].Value = "Rota";
@@ -29,58 +52,41 @@ namespace TraversalCoreProje.Controllers
 
             var bytes = excel.GetAsByteArray();
 
-            return File(bytes, "application/vnd.openxmlformats-ofiicedocument.spreadsheetml.sheet", "dosya1.xlsx");
+            return File(bytes, "application/vnd.openxmlformats-ofiicedocument.spreadsheetml.sheet", "dosya2.xlsx");
 
 
 
             return View();
         }
 
-        public List<DestinationModel> DestinationList()
+        public IActionResult DestinationExcelReport()
         {
-            List<DestinationModel> destinationModels = new List<DestinationModel>();
-            using (var c = new Context())
+            using (var workbook = new XLWorkbook())
             {
-                destinationModels = c.Destinations.Select(x => new DestinationModel
+                var workSheet = workbook.Worksheets.Add("Tur Listesi");
+                workSheet.Cell(1, 1).Value = "Şehir";
+                workSheet.Cell(1, 2).Value = "Konaklama Süresi";
+                workSheet.Cell(1, 3).Value = "Fiyat";
+                workSheet.Cell(1, 4).Value = "Kapasite";
+
+                int rowCount = 2;
+                foreach (var item in DestinationList())
                 {
-                    City = x.City,
-                    DayNight = x.DayNight,
-                    Price = x.Price,
-                    Capacity = x.Capacity
-
-                }).ToList();
-
-                return destinationModels;
-            }
-
-            public IActionResult DestinationExcelReport()
-            {
-                using (var workbook = new XLWorkbook())
-                {
-                    var workSheet = workbook.Worksheets.Add("Tur Listesi");
-                    workSheet.Cell(1, 1).Value = "Şehir";
-                    workSheet.Cell(1, 2).Value = "Konaklama Süresi";
-                    workSheet.Cell(1, 3).Value = "Fiyat";
-                    workSheet.Cell(1, 4).Value = "Kapasite";
-
-                    int rowCount = 2;
-                    foreach (var item in DestinationList())
-                    {
-                        workSheet.Cell(rowCount, 1).Value = item.City;
-                        workSheet.Cell(rowCount, 1).Value = item.DayNight;
-                        workSheet.Cell(rowCount, 1).Value = item.Price;
-                        workSheet.Cell(rowCount, 1).Value = item.Capacity;
-                    }
-
-                    using (var stream = new MemoryStream())
-                    {
-                        workbook.SaveAs(stream);
-                        var content = stream.ToArray();
-                        return File(content, "application/vnd.openxmlformats-ofiicedocument.spreadsheetml.sheet",  "YeniListe.xlsx");
-                    }
+                    workSheet.Cell(rowCount, 1).Value = item.City;
+                    workSheet.Cell(rowCount, 1).Value = item.DayNight;
+                    workSheet.Cell(rowCount, 1).Value = item.Price;
+                    workSheet.Cell(rowCount, 1).Value = item.Capacity;
                 }
-               
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-ofiicedocument.spreadsheetml.sheet", "YeniListe.xlsx");
+                }
             }
+
         }
     }
 }
+
