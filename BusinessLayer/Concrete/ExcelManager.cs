5b305cf134cc982613357afd3d8ec5fb5cd8aa69
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-10-04 22:49:06 +0300

    ExcelManager and ExcelService created

---
+++ BusinessLayer/Concrete/ExcelManager.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ExcelManager : IExcelService
+    {
+        public byte[] ExcelList<T>(List<T> t) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
