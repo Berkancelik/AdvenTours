1e9f14cc51e55a561e4963dc7c293f89051d5925
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-10-08 14:49:15 +0300

    Mail Controller template and configuration created

---
+++ TraversalCoreProje/Controllers/MailController.cs
@@ -0,0 +1,42 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAdressFrom = new MailboxAddress(mailRequest.Name, mailRequest.SenderMail); 
+
+            mimeMessage.From.Add(mailboxAdressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate(mailRequest.SenderMail, "passsword");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            
+               
+
+            return View();
+        }
+    }
+}
