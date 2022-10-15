61d190e4b4351e98f8779f5be8b21220af38eca3
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-10-15 22:15:23 +0300

    City listed completed

---
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(cities);
+            return Json(jsonCity);
+        }
+
+
+
+        public static List<CityClass> cities = new List<CityClass>()
+        {
+            new CityClass()
+            {
+                Id = 1,
+                Name ="Ardahan",
+                Country = "Türkiye"
+            },
+               new CityClass()
+            {
+                Id = 1,
+                Name ="Anakara",
+                Country = "Türkiye"
+            },
+                  new CityClass()
+            {
+                Id = 1,
+                Name ="Kars",
+                Country = "Türkiye"
+            }
+
+        };
+
+    }
+}
---
+++ TraversalCoreProje/Models/CityClass.cs
@@ -0,0 +1,9 @@
+﻿namespace TraversalCoreProje.Models
+{
+    public class CityClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
