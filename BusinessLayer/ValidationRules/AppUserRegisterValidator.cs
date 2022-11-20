246449b40c097690d03b158e566e8d4c3ffd1703
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-11-20 13:53:11 +0300

    AppUserRegisterValidation has benn completed

---
+++ BusinessLayer/ValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,21 @@
+﻿using DTOLayer.DTOs.AppUserDTOs;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDTOs>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail alanı boş geçilemez");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı adı alanı boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar alanı boş geçilemez");
+            RuleFor(x => x.UserName).MinimumLength(5).WithMessage("Lütfen en az 5 karakter girişi yapınız");
+            RuleFor(x => x.UserName).MaximumLength(25).WithMessage("Lütfen en fazla 25 karakter girişi yapınız");
+            RuleFor(x => x.Password).Equal(y => y.ConfirmPassword).WithMessage("Şifreler birbirleriyle uyuşmuyor");
+        }
+    }
+}
