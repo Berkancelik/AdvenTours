4665c7ac0e7a68d85fe484b5e926bff4f28a16b1
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-11-27 14:55:03 +0300

    Annocuement architecture completed

---
+++ BusinessLayer/Concrete/AnnoucementManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AnnoucementManager : IAnnoucementService
+    {
+        private readonly IAnnoucementDal _annoucementDal;
+
+        public AnnoucementManager(IAnnoucementDal annoucementDal)
+        {
+            _annoucementDal = annoucementDal;
+        }
+
+        public void TAdd(Annoucement t)
+        {
+            _annoucementDal.Insert(t);
+        }
+
+        public void TDelete(Annoucement t)
+        {
+            _annoucementDal.Delete(t);
+        }
+
+        public Annoucement TGetByID(int id)
+        {
+            return _annoucementDal.GetById(id);
+        }
+
+        public List<Annoucement> TGetList()
+        {
+            return _annoucementDal.GetList();
+        }
+
+        public void TUpdate(Annoucement t)
+        {
+            _annoucementDal.Update(t);
+        }
+    }
+}
---
+++ TraversalCoreProje/Controllers/AnnoucementControllercs.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class AnnoucementControllercs : Controller
+    {
+        private readonly IAnnoucementService _service;
+
+        public AnnoucementControllercs(IAnnoucementService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _service.TGetList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public IActionResult AddAnnoucement()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        public IActionResult AddAnnoucement(string x)
+        {
+            return View();
+        }
+
+    }
+}
