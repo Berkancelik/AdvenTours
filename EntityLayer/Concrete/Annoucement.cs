69c2738f81b9be3e8de563db7286c59735eb8309
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-11-27 14:20:24 +0300

    Annoucement entity created

---
+++ EntityLayer/Concrete/Annoucement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Annoucement
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Context { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
