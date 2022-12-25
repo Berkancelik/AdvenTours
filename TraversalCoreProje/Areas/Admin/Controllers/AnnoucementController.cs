bc84d8e3cb0b484189a2b1046f405721b9100700
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-12-25 19:25:01 +0300

    AnnoucementController Created

---
+++ TraversalCoreProje/Areas/Admin/Controllers/AnnoucementController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AnnoucementController : Controller
+    {
+        private readonly IAnnoucementService _annoucementService;
+
+        public AnnoucementController(IAnnoucementService annoucementService)
+        {
+            _annoucementService = annoucementService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _annoucementService.TGetList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public IActionResult AddAnnoucement()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddAnnoucement(stirng x)
+        {
+            return View();
+        }
+    }
+}
