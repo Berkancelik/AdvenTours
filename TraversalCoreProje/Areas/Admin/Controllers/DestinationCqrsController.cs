f38397ff7305dfb94d13ec1ea35d2d5600faefb5
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-01-03 19:10:29 +0300

    CQRS operation created

---
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationCqrsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    public class DestinationCqrsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProje/CQRS/Handlers/GetAllDestinationQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using System.Data.Entity;
+using System.Linq;
+using TraversalCoreProje.CQRS.Queries.DestinationQueries;
+using TraversalCoreProje.CQRS.Results.DestinationResults;
+
+namespace TraversalCoreProje.CQRS.Handlers
+{
+    public class GetAllDestinationQueryHandler
+    {
+        private readonly Context _context;
+
+        public GetAllDestinationQueryHandler(Context context)
+        {
+            _context = context;
+        }
+
+        public List<GetAllDestinationQueryResult> Handle()
+        {
+            var values = _context.Destinations.Select(x => new GetAllDestinationQueryResult
+            {
+                id = x.DestinationID,
+                capacity = x.Capacity,
+                city = x.City,
+                daynight = x.DayNight,
+                price = x.Price
+            }).AsNoTracking().ToList();
+            return values;
+        }
+    }
+}
