8884e0f547b8a068a713e4c8b489cef766e59f4c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-01-18 22:24:40 +0300

    DefaultController Index Created

--- TraversalCoreProje/Controllers/DefaultController.cs
+++ TraversalCoreProje/Controllers/DefaultController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Controllers
 {
+    [AllowAnonymous]
     public class DefaultController : Controller
     {
-
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
