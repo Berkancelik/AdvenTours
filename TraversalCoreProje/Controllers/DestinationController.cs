be86fd16bd16d95370c79f7439aae14cfda25bb9
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-01-21 17:37:46 +0300

    Refactoring

--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -1,11 +1,16 @@
 ﻿using BusinessLayer.Concrete;
-using DataAccessLayer.EntityFreamework;
+ using DataAccessLayer.EntityFreamework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace TraversalCoreProje.Controllers
 {
+    [AllowAnonymous]
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
@@ -14,18 +19,18 @@ namespace TraversalCoreProje.Controllers
             var values = destinationManager.TGetList();
             return View(values);
         }
-        [HttpGet]
+
+        //[HttpGet]
         public IActionResult DestinationDetails(int id)
         {
             ViewBag.i = id;
             var values = destinationManager.TGetByID(id);
             return View(values);
         }
-
-        [HttpPost]
-        public IActionResult DestinationDetails(Destination p)
-        {
-            return View();
-        }
+        //[HttpPost]
+        //public IActionResult DestinationDetails(Destination p)
+        //{
+        //    return View();
+        //}
     }
 }
