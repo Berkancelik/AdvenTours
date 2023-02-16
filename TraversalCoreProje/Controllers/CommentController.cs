8d9c77015d402a2885568f6cac120bce4498ee64
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-02-16 21:35:34 +0300

    User Comment created

--- TraversalCoreProje/Controllers/CommentController.cs
+++ TraversalCoreProje/Controllers/CommentController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFreamework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
@@ -8,21 +9,26 @@ namespace TraversalCoreProje.Controllers
 {
     public class CommentController : Controller
     {
-        /// <summary>
-        /// Method to return ad commnet values
-        /// </summary>
+
         CommentManager commentManager = new CommentManager(new EfCommentDal());
+        private readonly UserManager<AppUser> _userManager;
+
+        public CommentController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public PartialViewResult AddComment()
         {
+            //ViewBag.CommentId = id;
+            //var value =  _userManager.FindByNameAsync(User.Identity.Name);
+            //ViewBag.userId = value.Id;
+
             return PartialView();
         }
 
-        /// <summary>
-        /// Method to return ad commnet values
-        /// </summary>
-        /// <param name="p">Comment Parameter</param>
-        /// <returns></returns>
+
 
         [HttpPost]
         public IActionResult AddComment(Comment p)
--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -1,7 +1,10 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFreamework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace TraversalCoreProje.Controllers
 {
@@ -9,6 +12,13 @@ namespace TraversalCoreProje.Controllers
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        private readonly UserManager<AppUser> _userManager;
+
+        public DestinationController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             var values = destinationManager.TGetList();
@@ -16,9 +26,11 @@ namespace TraversalCoreProje.Controllers
         }
 
         //[HttpGet]
-        public IActionResult DestinationDetails(int id)
+        public async Task<IActionResult> DestinationDetails(int id)
         {
-            ViewBag.i = id;
+            ViewBag.destId = id;
+            var value = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userId = value.Id;
             var values = destinationManager.TGetByID(id);
             return View(values);
         }
