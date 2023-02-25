f0b2f0f7994544e6a445db3de0a47479c39ee12e
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-02-25 20:50:43 +0300

    .

--- SignalRApi/Controllers/VisitorController.cs
+++ SignalRApi/Controllers/VisitorController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SignalRApi.DAL;
+using SignalRApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SignalRApi.Controllers
 {
@@ -7,5 +13,30 @@ namespace SignalRApi.Controllers
     [ApiController]
     public class VisitorController : ControllerBase
     {
+        private readonly VisitorService _visitorService;
+        public VisitorController(VisitorService visitorService)
+        {
+            _visitorService = visitorService;
+        }
+        [HttpGet]
+        public IActionResult CreateVisitor()
+        {
+            Random random = new Random();
+            Enumerable.Range(1, 10).ToList().ForEach(x =>
+            {
+                foreach (ECity item in Enum.GetValues(typeof(ECity)))
+                {
+                    var newVisitor = new Visitor
+                    {
+                        City = item,
+                        CityVisitCount = random.Next(100, 2000),
+                        VisitDate = DateTime.Now.AddDays(x)
+                    };
+                    _visitorService.SaveVisitor(newVisitor).Wait();
+                    System.Threading.Thread.Sleep(1000);
+                }
+            });
+            return Ok("Ziyaretçiler başarılı bir şekilde eklendi");
+        }
     }
 }
--- SignalRApi/Model/VisitorChart.cs
+++ SignalRApi/Model/VisitorChart.cs
@@ -1,6 +1,15 @@
-﻿namespace SignalRApi.Model
+﻿using System.Collections.Generic;
+
+namespace SignalRApi.Model
 {
     public class VisitorChart
     {
+        public VisitorChart()
+        {
+            Counts = new List<int>();
+        }
+        public string VisitDate { get; set; }
+        public List<int> Counts { get; set; }
+
     }
 }
